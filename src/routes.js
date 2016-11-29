17835ccfcea09b4342def2ac1148e4036498efd1
denesnori <denes.nori@yahoo.com>
2016-11-29 17:01:18 +0000

    alter login to be get/post request

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,40 @@
 const sqlLogin = require('./dbrequests/login.js');
 
 const login = {
-  method: 'POST',
+  method: ['GET', 'POST'],
   path: '/login',
   config: {
-    handler: function (req, reply) {
-      const username = req.payload.username;
-      const password = req.payload.password;
-      console.log(username, password);
-      sqlLogin((err, data) => {
-        console.log('called sql');
-        if (err) { throw err; }
-        console.log(data);
-      }, username);
+    handler: function (request, reply) {
+      var message;
+      if (request.method === 'POST') {
+        const username = request.payload.username;
+        const password = request.payload.password;
+        if (!username || !password) {
+          message = 'Missing username or password';
+        }
 
-      let user = '';
-      console.log('hello');
-      if (user) {
-        req.cookieAuth.set(user);
-        reply('logged in'); // this should serve home page/ success page
+        /* console.log(username, password);
+        sqlLogin((err, data) => {
+          console.log('called sql');
+          if (err) { throw err; }
+          console.log(data);
+        }, username);
+
+        let user = '';
+        console.log('hello');
+        if (user) {
+          req.cookieAuth.set(user);
+          reply('logged in'); // this should serve home page/ success page
+        } */
+      }
+      if (request.method === 'get' ||
+       message) {
+        return reply('<html><head><title>Login page</title></head><body>' +
+           (message ? '<h3>' + message + '</h3><br/>' : '') +
+           '<form method="post" action="/login">' +
+           'Username: <input type="text" name="username"><br>' +
+           'Password: <input type="password" name="password"><br/>' +
+           '<input type="submit" value="Login"></form></body></html>');
       }
     }
   }
