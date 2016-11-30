805a4603681e7b050e28e6eefcdebfd8ee166e06
Nick Field <heyimnick@live.com>
2016-11-30 12:48:49 +0000

    refactored loginhandler, refactor login.js

--- src/dbrequests/login.js
+++ src/dbrequests/login.js
@@ -2,6 +2,6 @@ const dbConn = require('../dbconnection.js');
 
 module.exports = (cb, username) => {
   dbConn.query(`SELECT * FROM users WHERE username = '${username}'`, (err, data) => {
-    (err ? cb(err) : cb(null, data.rows));
+    (err ? cb(err) : cb(null, data.rows[0]));
   });
 };
