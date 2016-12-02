361f96121ac7c60f3a94d54b640a0ec65c3fbaed
emily <limeyb7@gmail.com>
2016-12-02 12:35:54 +0000

    rename home handler with camel case

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const login = require('./handlers/loginHandler.js');
-const home = require('./handlers/homehandler.js');
+const homepage = require('./handlers/homeHandler.js');
 const resources = require('./handlers/resourcesHandler.js');
 const resourceProfile = require('./handlers/resourceProfile.js');
 const userProfile = require('./handlers/userProfile.js');
@@ -7,19 +7,19 @@ const addReview = require('./handlers/addReviewHandler.js');
 const edit = require('./handlers/editReviewHandler.js');
 const deleteReview = require('./handlers/deleteReviewHandler.js');
 const newUser = require('./handlers/newUserHandler.js');
-const file = require('./handlers/filehandler.js');
+const staticFiles = require('./handlers/fileHandler.js');
 const logout = require('./handlers/logout.js');
 
 module.exports = [
   login,
   logout,
-  home,
+  homepage,
   resources,
   resourceProfile,
   addReview,
   deleteReview,
   edit,
-  file,
+  staticFiles,
   userProfile,
   newUser
 ];
